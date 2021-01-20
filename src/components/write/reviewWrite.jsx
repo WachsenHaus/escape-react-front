0f24d89514f86dccd4aef257da3b09831dfc770f
Younghoon CHOI _^ <WachsenHaus@gmail.com>
2021-01-20 23:36:35 +0900

    <추가>: <ckeditor5 이미지 업로드 적용중> 💨 이미지가 업로드되게 서버단을 수정하였으나, 업로드는 되는데 에디터에서 표시가 안됨. img의 src를 보면 /escape/img/파일이름 이렇게 나오고있는데 서버는 8888포트이고.. 지금 프론트 서버는 3000포트임. 추후 통합시키면 문제는안되지만. 현재로서 해결은어떻게하지?

--- src/components/write/reviewWrite.jsx
+++ src/components/write/reviewWrite.jsx
@@ -1,39 +1,132 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Button, Container, Table } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
+import React from "react";
+import { Container } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+// import styles from "./reviewWrite.module.css";
+import styles from "./content-styles.css";
+import { Button } from "react-bootstrap";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
+import Autosave from "@ckeditor/ckeditor5-autosave/src/autosave.js";
+import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder.js";
+import CKFinderUploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js";
+import ExportToPDF from "@ckeditor/ckeditor5-export-pdf/src/exportpdf.js";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
+import Image from "@ckeditor/ckeditor5-image/src/image.js";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import List from "@ckeditor/ckeditor5-list/src/list.js";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import SpecialCharacters from "@ckeditor/ckeditor5-special-characters/src/specialcharacters.js";
+import SpecialCharactersEssentials from "@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
+import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
 
 const EditorReview = (props) => {
+  const installedPlugins = [
+    Alignment,
+    Autoformat,
+    Autosave,
+    BlockQuote,
+    Bold,
+    CKFinder,
+    CKFinderUploadAdapter,
+    Essentials,
+    ExportToPDF,
+    FontColor,
+    FontFamily,
+    FontSize,
+    Heading,
+    HorizontalLine,
+    Image,
+    ImageCaption,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    List,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    SpecialCharacters,
+    SpecialCharactersEssentials,
+    Strikethrough,
+    Table,
+    TableToolbar,
+    TextTransformation,
+    Underline,
+  ];
+
   return (
     <>
+      <link rel="stylesheet" href="./content-styles.css" type="text/css"></link>
       <Container>
-        <CKEditor
-          style={{ color: "black" }}
-          editor={ClassicEditor}
-          config={{
-            plugins: [Paragraph, Bold, Italic, Essentials],
-            toolbar: ["bold", "italic"],
-          }}
-          onReady={(editor) => {
-            // You can store the "editor" and use when it is needed.
-            console.log("Editor is ready to use!", editor);
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(event, editor) => {
-            console.log("Blur.", editor);
-          }}
-          onFocus={(event, editor) => {
-            console.log("Focus.", editor);
-          }}
-        />
+        <div>
+          <CKEditor
+            editor={ClassicEditor}
+            config={{
+              plugins: [...installedPlugins],
+              ckfinder: {
+                uploadUrl: "http://localhost:8888/escape/review/imgUpload.do",
+              },
+              toolbar: [
+                "fontFamily",
+                "fontSize",
+                "fontColor",
+                "alignment",
+                "|",
+                "bold",
+                "italic",
+                "strikethrough",
+                "underline",
+                "specialCharacters",
+                "horizontalLine",
+                "|",
+                "bulletedList",
+                "numberedList",
+                "|",
+                "indent",
+                "outdent",
+                "|",
+                "link",
+                "blockQuote",
+                "imageUpload",
+                "mediaEmbed",
+                "|",
+                "undo",
+                "redo",
+              ],
+            }}
+            data="<p>다른 사용자를 위해 테마 내용은 지양해주세요</p>"
+          />
+        </div>
+        <Button>글쓰기</Button>
       </Container>
     </>
   );
